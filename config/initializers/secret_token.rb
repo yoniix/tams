d8e453a590b40258295125a40bda36e6eb5c2a86
Yonas Bedasso <yonas.bedasso@gmail.com>
2013-01-24 22:30:10 -0600

    Initial commit for tams

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tams::Application.config.secret_token = 'bd1da4b9766a408d5f678266d7d74e1e011d50b27bd4c856bfbdcb80d8fdf4960c8ac1463ed59707f46f3daa2c16c4b8d83eb0966dd68393aa762d01e263985c'
