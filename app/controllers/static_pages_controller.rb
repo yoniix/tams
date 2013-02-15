d24e4e99d98a176440237b1190a4a780feaf87fe
Yonas Bedasso <yonas.bedasso@gmail.com>
2013-02-15 17:32:32 -0600

    setting up basic layout

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,15 @@
+class StaticPagesController < ApplicationController
+  def home
+    #@post = current_user.posts.build if signed_in?
+  end
+
+  def help
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+end
